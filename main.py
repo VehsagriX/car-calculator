2650241691a6cd4ad4f628945c74d0d32d2d80b6
vehsagri97 <122737191+vehsagri97@users.noreply.github.com>
2024-02-18 13:41:41 +0500

    Created project car_calculator

---
+++ main.py
@@ -0,0 +1,16 @@
+import calculator
+if __name__ == '__main__':
+    calc = calculator.Calculator()
+    calc.add_car(
+        calculator.Car('Toyota Corolla', 30000, 7, 1200, 2500),
+    )
+    calc.add_car(
+        calculator.ElectricCar('Tesla Model 3', 200000, 5500, 150),
+    )
+    calc.add_car(
+        calculator.Car('Range Rover', 650000, 3, service_cost=3000, insurance_cost=7000),
+    )
+
+
+
+calc.print_cars()
